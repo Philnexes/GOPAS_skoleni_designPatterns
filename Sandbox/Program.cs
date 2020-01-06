108b8c0c2ba6db33e03433f1a10d025d1d4ec2bd
Filip Dorian Pindej <filip.pindej@gmail.com>
2020-01-06 14:29:03 +0100

    Implemented AutoFAC container

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,4 +1,5 @@
-﻿using Ninject;
+﻿using Autofac;
+using Ninject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,12 +12,21 @@ namespace Sandbox
     {
         static void Main(string[] args)
         {
-            var container = new StandardKernel();
+            /*var container = new StandardKernel();
             container.Bind<IB>().To<B>();
             var a1 = container.Get<A>();
             var a2 = container.Get<A>();
             a1.UseB();
             a2.UseB();
+            Console.WriteLine(a1.B == a2.B);*/
+            var builder = new ContainerBuilder();
+            builder.RegisterType<A>();
+            builder.RegisterType<B>().As<IB>();
+            var container = builder.Build();
+
+            var a1 = container.Resolve<A>();
+            var a2 = container.Resolve<A>();
+
             Console.WriteLine(a1.B == a2.B);
         }
     }
